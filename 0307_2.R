a38ef0f065c77e2763a598a32c7b691d7407c0be
HedyTseng0530 <hebe0530@yahoo.com.tw>
2016-03-10 04:47:51 +0800

    Class 2

---
+++ 0307_2.R
@@ -0,0 +1,24 @@
+q<-c(1.4:5.6)
+x<-c(1:5)
+x+3
+seq(1,9)
+seq(1,9,by=2)
+rep(1,9)
+
+s<-1:30
+sum(s)
+mean(s)
+max(s)
+min(s)
+median(s)
+round(sd(s),3)
+summary(s)
+sort(s)
+sort(s,decreasing = T)
+rev(s)
+
+matrix(2,3)
+matrix(nrow=2,ncol=3)
+matrix(1:6,nrow=2,ncol=3)
+matrix("a",nrow=2,ncol=3)
+dim(matrix(1:6,nrow=2,ncol=3))
---
+++ README.md
@@ -0,0 +1,10 @@
+#長庚大學資管系 大數據分析方法 作業二
+##我很乖，我有交作業
+##我很乖，我有在電腦安裝好軟體
+
+###RStudio及GitHub安裝完成截圖
+![alt text](http://imgur.com/PaBvSBm)
+###RStudio 程式畫面
+![alt text](http://imgur.com/Cb216FS)
+###GitHub 程式畫面
+![alt text](http://imgur.com/uokdcir)
